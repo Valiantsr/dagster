a1222e08ade7a663a83506742ea2106886526672
Valiant <valiant.shabri@gmail.com>
2024-08-22 09:18:35 +0700

    Update ci_cd and test

--- test_model.py
+++ test_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import mlflow.xgboost
 
 # Load testing data
 testing_data = pd.read_csv('testing_data.csv')
@@ -8,7 +9,9 @@ X_test = testing_data.drop('Target', axis=1)
 y_test = testing_data['Target']
 
 # Load the model
-model = mlflow.sklearn.load_model("model")
+model_name = "XGB-smote"
+model_uri = f"models:/{model_name}@challenger"
+model = mlflow.xgboost.load_model(model_uri)
 
 # Evaluate the model
 y_pred = model.predict(X_test)
