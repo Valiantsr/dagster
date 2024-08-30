bdb557f8bc848a0d7a075420d3e66faa4598a3e6
Valiant <valiant.shabri@gmail.com>
2024-08-30 15:11:08 +0700

    Update

--- sentiment_analysis/test_model_sentiment.py
+++ sentiment_analysis/test_model_sentiment.py
@@ -21,7 +21,7 @@ if not os.path.exists(local_path):
         f.write(response.content)
 
 # Load test dataset
-test_data = pd.read_csv(local_path, delimiter=',')
+test_data = pd.read_csv(local_path, error_bad_lines=False)
 texts = test_data['text'].tolist()
 true_labels = test_data['label'].tolist()
 
