0b8b540a2c594bbe0874e3dc1a8108ef2589d238
Valiant <valiant.shabri@gmail.com>
2024-08-30 09:27:02 +0700

    Update

--- sentiment_analysis/test_model_sentiment.py
+++ sentiment_analysis/test_model_sentiment.py
@@ -2,10 +2,22 @@ import os
 import mlflow
 from transformers import BertTokenizer, BertForSequenceClassification
 import pandas as pd
+import requests
 
 os.environ['MLFLOW_TRACKING_URI'] = 'https://dagshub.com/valiant.shabri/dagster.mlflow'
 os.environ['MLFLOW_TRACKING_USERNAME'] = 'valiant.shabri'
 os.environ['MLFLOW_TRACKING_PASSWORD'] = 'd37b33ad4e0564f52162d90248e477d373a699f1'
+
+# URL untuk mengunduh test_data.csv dari DagsHub
+url = "https://dagshub.com/valiant.shabri/dagster/raw/main/s3:/dagster/data/test_data.tsv"
+local_path = 'sentiment_analysis/datasets/test_data.tsv'
+
+# Jika file belum ada di direktori lokal, unduh dari DagsHub
+if not os.path.exists(local_path):
+    response = requests.get(url)
+    os.makedirs(os.path.dirname(local_path), exist_ok=True)
+    with open(local_path, 'wb') as f:
+        f.write(response.content)
 
 # Load test dataset
 test_data = pd.read_csv('sentiment_analysis/datasets/test_data.csv')
